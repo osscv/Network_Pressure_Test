4252d04dd5e5e2eccc05eadf42ce67bbe881167c
osscv <140930559+osscv@users.noreply.github.com>
2024-12-27 11:17:56 +0800

    pressure_test.py

--- pressure_test.py
+++ pressure_test.py
@@ -1,3 +1,15 @@
+"""
+Network Bandwidth Pressure Tester
+Copyright (C) 2024 www.dkly.top
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Repository: https://github.com/osscv/Network_Pressure_Test
+"""
+
 import requests
 import time
 import threading
